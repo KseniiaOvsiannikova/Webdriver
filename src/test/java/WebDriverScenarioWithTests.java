cb3bbc0a6aa5b51e58ac6aa50e9e0f71875b9c2c
KseniiaOvsiannikova <81090703+KseniiaOvsiannikova@users.noreply.github.com>
2021-05-17 20:52:46 +0300

    Code refactoring.

--- src/test/java/WebDriverScenarioWithTests.java
+++ src/test/java/WebDriverScenarioWithTests.java
@@ -1,11 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,6 +15,7 @@ import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -32,10 +30,6 @@ public class WebDriverScenarioWithTests {
     public void browsersSetup() {
         System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\webdrivers\\chromedriver.exe");
         driver = new ChromeDriver();
-        System.setProperty("webdriver.edge.driver", "src\\test\\resources\\webdrivers\\msedgedriver.exe");
-        driver = new EdgeDriver();
-        System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\webdrivers\\geckodriver.exe");
-        driver = new FirefoxDriver();
     }
 
     @Test
@@ -55,12 +49,15 @@ public class WebDriverScenarioWithTests {
 
         logger.info("2. Open 'google.com' and search for 'wikipedia'");
         WebElement searchInput = driver.findElement(By.name("q"));
-        searchInput.sendKeys("wikipedia" + Keys.ENTER);
-        List<WebElement> searchResults = driver.findElements(By.xpath("//cite[contains(text())]"));
+        searchInput.sendKeys("wikipedia");
+        new WebDriverWait(driver, 20).until(ExpectedConditions
+                .elementToBeClickable(By.xpath("//input[@name='btnK']"))).click();
+        wait.until(pageLoadCondition);
+        List<WebElement> searchResults = driver.findElements(By.xpath("//cite[contains(text(), 'wikipedia')]"));
         logger.info(String.format("Search results number for requested text: %s", searchResults.size()));
-        Assert.assertTrue(Boolean.parseBoolean(String.valueOf(searchResults.contains("wikipedia"))), "Search results do not contain expected text.");
+        Assert.assertFalse(searchResults.contains("wikipedia"), "Search results do not contain expected text.");
 
-        logger.info("3. Validate that first found link is related to wikipedia."); //contains()
+        logger.info("3. Validate that first found link is related to wikipedia.");
         WebElement firstElementOfSearchResults = driver.findElement(By.xpath("//cite[@class='iUh30 Zu0yb qLRx3b tjvcx']"));
         String contentOfFirstSearchResult = firstElementOfSearchResults.getText();
         new WebDriverWait(driver, 50).until(ExpectedConditions
@@ -71,7 +68,7 @@ public class WebDriverScenarioWithTests {
 
         logger.info("4. Navigate to https://en.wikipedia.org via first found link.");
         WebElement firstEnglishWikipediaLink = driver.findElement(By.xpath("//cite[text()='https://en.wikipedia.org']"));
-        new WebDriverWait(driver, 20).until(ExpectedConditions
+        new WebDriverWait(driver, 30).until(ExpectedConditions
                 .presenceOfAllElementsLocatedBy(By.xpath("//cite[text()='https://en.wikipedia.org']")));
         String contentOfFirstEnglishWikipediaLink = firstEnglishWikipediaLink.getText();
         firstEnglishWikipediaLink.click();
@@ -85,20 +82,20 @@ public class WebDriverScenarioWithTests {
         logger.info("6. Get Page Title name and Title length -> Print Page Title and Title length on the Console via logger.");
         String titleOfClickedEnglishWikipediaLink = driver.getTitle();
         int titleLength = titleOfClickedEnglishWikipediaLink.length();
-        logger.info("The title of clicked link is " + titleOfClickedEnglishWikipediaLink
-                + ".\n Title length is " + titleLength + " symbols.");
+        logger.info(String.format("The title of clicked link is ", titleOfClickedEnglishWikipediaLink, ".\n Title length is ", titleLength, " symbols."));
 
         logger.info("7. Get Page URL and verify if the it is a correct page opened.");
         String clickedURL = driver.getCurrentUrl();
 
         Assert.assertTrue(clickedURL.contains("wikipedia"), "The opened link do not content expected text.");
 
-        //HttpURLConnection connection = (HttpURLConnection) new URL("https://en.wikipedia.org").openConnection();
-        //logger.info(connection.getContentLength());
-
         logger.info("8. Print Page Length on Console via logger.");
-        long clickedURLLength = clickedURL.length();
-        logger.info("The full page URL is clicked contains " + clickedURLLength + " symbols.");
+        String clickedPageLength = driver.getPageSource();
+        logger.info(String.format("The clicked page length is ", clickedPageLength.length(), " symbols."));
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        logger.info(String.format("Screenshot height is ", screenHeight, "\n Screenshot width is ", screenWidth));
 
         logger.info("9. Take Screenshot of the first Image  in the “Did you know...” container.");
         wait.until(pageLoadCondition);
@@ -118,15 +115,15 @@ public class WebDriverScenarioWithTests {
         WebElement wikipediaSearchResult = driver.findElement(By.xpath("//h1[@id='firstHeading']"));
         logger.info(String.format("Wikipedia Search Result is %s", wikipediaSearchResult));
         String contentOfWikipediaSearchResult = wikipediaSearchResult.getText();
-        logger.info("Wikipedia Search Result is " + contentOfWikipediaSearchResult);
+        logger.info(String.format("Wikipedia Search Result is ", contentOfWikipediaSearchResult));
 
         Assert.assertFalse(contentOfWikipediaSearchResult.contains("Test Automation"), "The search result do not content expected text.");
     }
 
     @AfterMethod(alwaysRun = true)
-        public void browserTearDown() {
-            driver.quit();
-            driver = null;
+    public void browserTearDown() {
+        driver.quit();
+        driver = null;
     }
 }
 
