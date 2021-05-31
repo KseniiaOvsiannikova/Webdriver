44d91b4f34918b61dbc938f2422dc4c81fde3b08
KseniiaOvsiannikova <81090703+KseniiaOvsiannikova@users.noreply.github.com>
2021-05-31 21:45:53 +0300

    Replacing StackoverflowPage.java and StackoverflowSearchResultsTest.java by TutorialspointPage.java and TutorialspointSearchResultsTest.java.

---
+++ src/test/java/pages/TutorialspointPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TutorialspointPage extends AbstractPage {
+
+    private static final String TUTORIALSPOINT_URL = "https://www.tutorialspoint.com/";
+
+    @FindBy(id = "gsc-i-id1")
+    private WebElement searchInputArea;
+
+    @FindBy(xpath = "//div[contains (text(), 'results')]")
+    private WebElement searchResults;
+
+    public TutorialspointPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public TutorialspointPage openPage() {
+        driver.get(TUTORIALSPOINT_URL);
+        driver.manage().window().maximize();
+        waitForPageLoad(driver);
+
+        return this;
+    }
+
+    public TutorialspointPage searchForPhrase() {
+        String searchValue = "submit() vs click()";
+        waitForPageLoad(driver);
+        searchInputArea.sendKeys(searchValue, Keys.ENTER);
+
+        return new TutorialspointPage(driver);
+    }
+
+    public int countTutorialspointSearchResults() {
+        waitForPageLoad(driver);
+        String searchResultsText = searchResults.getText();
+        return Integer.parseInt(searchResultsText.replaceAll("[\\D]", ""));
+    }
+
+}
---
+++ src/test/java/tests/TutorialspointSearchResultsTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TutorialspointPage;
+
+public class TutorialspointSearchResultsTest extends BaseClass {
+
+    @Test
+    public void searchResultsNumberIsGraterThanHundred() {
+
+        logger.info("Test 'searchResultsNumberIsGraterThanHundred' started.");
+
+        int expectedTutorialspointSearchResultsNumber = new TutorialspointPage(driver)
+                .openPage()
+                .searchForPhrase()
+                .countTutorialspointSearchResults();
+
+        logger.info(String.format("Search results number for requested text: %s", expectedTutorialspointSearchResultsNumber));
+        Assert.assertTrue(expectedTutorialspointSearchResultsNumber > 100, "Search results number is lower than 100.");
+    }
+}
