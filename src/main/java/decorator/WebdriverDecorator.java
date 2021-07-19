2e87fa0c0b05b383a05c3b9a93f0fe19c1932085
KseniiaOvsiannikova <81090703+KseniiaOvsiannikova@users.noreply.github.com>
2021-07-19 17:47:48 +0300

    Adding logger to WebdriverDecorator.java, refactoring pom.xml.

--- src/main/java/decorator/WebdriverDecorator.java
+++ src/main/java/decorator/WebdriverDecorator.java
@@ -2,71 +2,76 @@ package decorator;
 
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Reporter;
 
+
 public class WebdriverDecorator implements WebDriver{
 
-    protected WebDriver decoratedDriver;
+    protected WebDriver driver;
+    protected Logger logger = Logger.getGlobal();
 
     public WebdriverDecorator (WebDriver driver) {
-        this.decoratedDriver = driver;
+        this.driver = driver;
     }
 
     public void get(String url) {
-        decoratedDriver.get(url);
+        driver.get(url);
     }
 
     public String getCurrentUrl() {
-        return decoratedDriver.getCurrentUrl();
+        return driver.getCurrentUrl();
     }
 
     public String getTitle() {
-        return decoratedDriver.getTitle();
+        logger.info("Using decoreted method.");
+
+        return driver.getTitle();
     }
 
     public List<WebElement> findElements(By by) {
-        return decoratedDriver.findElements(by);
+        return driver.findElements(by);
     }
 
     public WebElement findElement(By by) {
-        Reporter.log(String.format("Finding element: %s, current URL: '%s'", by.toString(), decoratedDriver.getCurrentUrl()),
+        Reporter.log(String.format("Finding element: %s, current URL: '%s'", by.toString(), driver.getCurrentUrl()),
                 true);
-        return decoratedDriver.findElement(by);
+        return driver.findElement(by);
     }
 
     public String getPageSource() {
-        return decoratedDriver.getPageSource();
+        return driver.getPageSource();
     }
 
     public void close() {
-        decoratedDriver.close();
+        driver.close();
     }
 
     public void quit() {
-        decoratedDriver.quit();
+        driver.quit();
     }
 
     public Set<String> getWindowHandles() {
-        return decoratedDriver.getWindowHandles();
+        return driver.getWindowHandles();
     }
 
     public String getWindowHandle() {
-        return decoratedDriver.getWindowHandle();
+        return driver.getWindowHandle();
     }
 
     public TargetLocator switchTo() {
-        return decoratedDriver.switchTo();
+        return driver.switchTo();
     }
 
     public Navigation navigate() {
-        return decoratedDriver.navigate();
+        return driver.navigate();
     }
 
     public Options manage() {
-        return decoratedDriver.manage();
+        return driver.manage();
     }
 }
