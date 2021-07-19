6455ca52be51725dba164a0831fbc12c1742d221
KseniiaOvsiannikova <81090703+KseniiaOvsiannikova@users.noreply.github.com>
2021-07-19 10:46:25 +0300

    Removing deprecated API usage in WebdriverDecoratorGoogleTest.java.

--- src/test/java/decorator/WebdriverDecoratorGoogleTest.java
+++ src/test/java/decorator/WebdriverDecoratorGoogleTest.java
@@ -1,8 +1,6 @@
 package decorator;
 
 import baseTest.BaseTest;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,8 +12,6 @@ public class WebdriverDecoratorGoogleTest extends BaseTest {
     @Test
     public void webdriverDecoratorTest() {
 
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        driver = new ChromeDriver(capabilities);
         driver = new WebdriverDecorator(driver);
         driver.navigate().to(fromPropertyGOOGLE_URL());
         String actualGoogleTitle = driver.getTitle();
